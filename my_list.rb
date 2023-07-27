e14055d73297d658608eb995c0756822ebb17653
Sandeep Ghosh <mailsg0912@gmail.com>
2023-07-27 15:59:19 +0530

    Implement CR's instructions

--- my_list.rb
+++ my_list.rb
@@ -7,14 +7,21 @@ class MyList
     @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
+  def each
+    return to_enum(:each) unless block_given?
+
+    counter = 0
+    while counter < @list.length
+      yield(@list[counter])
+      counter += 1
+    end
+    @list
   end
 end
 
 list = MyList.new(1, 2, 3, 4)
-p(list.all { |e| e < 5 })
-p(list.all { |e| e > 5 })
-p(list.any { |e| e == 2 })
-p(list.any { |e| e == 5 })
+p(list.all? { |e| e < 5 })
+p(list.all? { |e| e > 5 })
+p(list.any? { |e| e == 2 })
+p(list.any? { |e| e == 5 })
 p(list.filter(&:even?))
